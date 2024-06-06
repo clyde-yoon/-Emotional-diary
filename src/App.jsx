b39726cef3ecc0e598765d6be4e732a621f94f81
clyde-yoon <a1056719@gmail.com>
2024-06-06 22:28:43 +0900

    🎉 감정일기장 프로젝트 셍성

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+ //1. "/" 모든 일기를 조회하는 홈페이지
+ //2. "/new" 새로운 일기를 작성하는 New페이지
+ //3. "/diary" 일기를 상세히 조회하는 Diary 페이지
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
