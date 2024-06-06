75d3933779625514f3505b98bbfab8dd597c97ee
clyde-yoon <a1056719@gmail.com>
2024-06-06 23:40:20 +0900

    Link 와 USeNavigate 사용해보기

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,21 @@ import Diary from './pages/Diary'
 import Home from './pages/Home'
 import New from './pages/New'
 import NotFound from './pages/Notfound'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 function App() {
+  const nav = useNavigate();
+
+  const onClickButton = ()=>{
+    nav('/new');
+  }
   return (
     <>
     <div>
-      <Link to={'/'}>Home</Link>
+      <Link to={'/'}>Home</Link>s
       <Link to={'/new'}>New</Link>
       <Link to={'/diary'}>Diary</Link>
     </div>
+    <button onClick={onClickButton}>New 페이지로 이동</button>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/new' element={<New/>}/>
