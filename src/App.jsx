84109854804d2c3d5422280c70d2b7d93cc2cb65
clyde-yoon <a1056719@gmail.com>
2024-06-07 22:09:53 +0900

    이미지 최적화

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ import Home from './pages/Home'
 import New from './pages/New'
 import NotFound from './pages/Notfound'
 import { Routes, Route, Link, useNavigate } from 'react-router-dom'
+import emotion1 from './assets/emotion1.png'
+import emotion2 from './assets/emotion2.png'
+import emotion3 from './assets/emotion3.png'
+import emotion4 from './assets/emotion4.png'
+import emotion5 from './assets/emotion5.png'
+
 function App() {
   const nav = useNavigate();
 
@@ -13,6 +19,13 @@ function App() {
   }
   return (
     <>
+    <div>
+      <img src={emotion1} alt="" />
+      <img src={emotion2} alt="" />
+      <img src={emotion3} alt="" />
+      <img src={emotion4} alt="" />
+      <img src={emotion5} alt="" />
+    </div>
     <div>
       <Link to={'/'}>Home</Link>s
       <Link to={'/new'}>New</Link>
